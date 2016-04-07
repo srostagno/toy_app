8004d7592802981725cc6ab9491c381179248c1a
“srostagno” <srostagno@gmail.comssh-keygen -t rsa -C srostagno@gmail.comcat>
2016-04-07 17:22:45 -0300

    Add bitch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-   render text: "hello, world!"
+   render text: "hello, biatch!"
  end
 
 end
