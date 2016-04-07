c2c6d01f17a1fbfdd226d9bf7eb4f975a9f847c3
“srostagno” <srostagno@gmail.comssh-keygen -t rsa -C srostagno@gmail.comcat>
2016-04-07 17:21:12 -0300

    Biatch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  def
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Hello Motherfucker!"
-  end
+   render text: "hello, world!"
+ end
+
 end
